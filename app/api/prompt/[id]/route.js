41b490749bab43a606f0d5aa6af68707701db043
JeronimoGinaca <jeronimo.ginaca@globant.com>
2023-07-28 16:21:04 +0200

    fist commit

---
+++ app/api/prompt/[id]/route.js
@@ -0,0 +1,60 @@
+import Prompt from "@models/prompt";
+import { connectToDB } from "@utils/database";
+
+// Get, leer
+
+export const GET = async (request, { params }) => {
+    try {
+        await connectToDB()
+
+        const prompt = await Prompt.findById(params.id).populate("creator")
+        if (!prompt) return new Response("Prompt Not Found", { status: 404 });
+
+        return new Response(JSON.stringify(prompt), { status: 200 })
+
+    } catch (error) {
+        return new Response("Internal Server Error", { status: 500 });
+    }
+}
+
+// PATCH, editar
+
+export const PATCH = async (request, { params }) => {
+    const { prompt, tag } = await request.json();
+
+    try {
+        await connectToDB();
+
+        // Encuentra una prompt por id
+        const existingPrompt = await Prompt.findById(params.id);
+
+        if (!existingPrompt) {
+            return new Response("Prompt not found", { status: 404 });
+        }
+
+        // actualiza la prompt con nueva data
+        existingPrompt.prompt = prompt;
+        existingPrompt.tag = tag;
+
+        await existingPrompt.save();
+
+        return new Response("Successfully updated the Prompts", { status: 200 });
+    } catch (error) {
+        return new Response("Error Updating Prompt", { status: 500 });
+    }
+};
+
+// DELETE, borrar
+
+export const DELETE = async (request, { params }) => {
+    try {
+        await connectToDB();
+
+        // Encuentra una prompt por id y la elimina
+        await Prompt.findByIdAndRemove(params.id);
+
+        return new Response("Prompt deleted successfully", { status: 200 });
+    } catch (error) {
+        return new Response("Error deleting prompt", { status: 500 });
+    }
+};
